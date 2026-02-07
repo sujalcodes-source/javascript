86e94d34da770d68467d024ca87afe8e535e7920
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-02-07 21:53:00 +0545

    Array code in js

---
+++ array/loopnarray.js
@@ -0,0 +1,5 @@
+let marks=[23,45,65,78,65,89,67];
+for(let idx=0;idx<marks.length;idx++)
+{
+    console.log(marks[idx]);
+}
--- array/marks.js
+++ array/marks.js
@@ -1,2 +1,3 @@
 let marks=[12,34,56,78,54];
 console.log(marks);
+console.log(marks[2]);
